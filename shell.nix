f88553c1109c4d469111bb84502248d793e1aa04
BeaverNotACat <beavernotacat@gmail.com>
2026-03-03 15:15:30 +0000

    Flattern asset in shell.nix

--- shell.nix
+++ shell.nix
@@ -11,30 +11,28 @@
 let
   system = pkgs.stdenv.hostPlatform.system;
 
-  assets = {
-    "x86_64-linux" = {
-      name = "wasi-sdk-30.0-x86_64-linux.tar.gz";
-      sha256 = "0507679dff16814b74516cd969a9b16d2ced1347388024bc7966264648c78bfb";
-    };
-    "aarch64-linux" = {
-      name = "wasi-sdk-30.0-arm64-linux.tar.gz";
-      sha256 = "0hzxsk3q025bzrv2jvni15yb96g9njdqpk32ck1df7a1gmxqhmwf";
-    };
-    "aarch64-darwin" = {
-      name = "wasi-sdk-30.0-arm64-macos.tar.gz";
-      sha256 = "2c2ed99296857e60fd14c3f40fe226231f296409502491094704089c31a16740";
-    };
-  };
+  name =
+    {
+      "x86_64-linux" = "wasi-sdk-30.0-x86_64-linux.tar.gz";
+      "aarch64-linux" = "wasi-sdk-30.0-arm64-linux.tar.gz";
+      "aarch64-darwin" = "wasi-sdk-30.0-arm64-macos.tar.gz";
+    }
+    .${system} or (throw "Unsupported system: ${system}");
 
-  asset =
-    if builtins.hasAttr system assets then assets.${system} else throw "Unsupported system: ${system}";
+  sha256 =
+    {
+      "x86_64-linux" = "0507679dff16814b74516cd969a9b16d2ced1347388024bc7966264648c78bfb";
+      "aarch64-linux" = "0hzxsk3q025bzrv2jvni15yb96g9njdqpk32ck1df7a1gmxqhmwf";
+      "aarch64-darwin" = "2c2ed99296857e60fd14c3f40fe226231f296409502491094704089c31a16740";
+    }
+    .${system} or (throw "Unsupported system: ${system}");
 
   wasiSdk = pkgs.stdenv.mkDerivation {
     pname = "wasi-sdk";
     version = "30.0";
     src = builtins.fetchTarball {
-      url = "https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-30/${asset.name}";
-      sha256 = asset.sha256;
+      url = "https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-30/${name}";
+      sha256 = sha256;
     };
 
     dontBuild = true;
